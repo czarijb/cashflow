ed3afff4247b8eb3d0699590733637a4f8663b67
Alexander Tsaregorodtsev <czar_ijb@mail.ru>
2017-01-05 18:21:27 +0700

    little bit beter

--- src/main/java/com/github/czarijb/MainFrame.java
+++ src/main/java/com/github/czarijb/MainFrame.java
@@ -12,7 +12,7 @@ public class MainFrame extends JFrame {
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         setSize(800, 600);
-        //setResizable(false);
+        setResizable(false);
         setLocationRelativeTo(null);
         setLayout(new GridBagLayout());
 
@@ -27,32 +27,32 @@ public class MainFrame extends JFrame {
                 new Insets(2, 2, 2, 2), 0, 0));
 
         AssetsTableModel assetsTableModel = new AssetsTableModel();
-        incomeTableModel.addIncomeDate(new Assets("Активы", 1000, 200));
+        assetsTableModel.addAssetsDate(new Assets("Активы", 1000, 200));
         JTable assetsTable = new JTable(assetsTableModel);
         JScrollPane assetsTableScrollPane = new JScrollPane(assetsTable);
         incomeTableScrollPane.setPreferredSize(new Dimension(265, 195));
 
-        add(assetsTableScrollPane, new GridBagConstraints(0, 0, 1, 1, 1, 1,
+        add(assetsTableScrollPane, new GridBagConstraints(0, 400, 1, 1, 1, 1,
                 GridBagConstraints.WEST, GridBagConstraints.VERTICAL,
                 new Insets(2, 2, 2, 2), 0, 0));
 
         LiabilitiesTableModel liabilitiesTableModel = new LiabilitiesTableModel();
-        incomeTableModel.addIncomeDate(new Liabilities("Пассивы", 200));
+        liabilitiesTableModel.addLiabilitiesDate(new Liabilities("Пассивы", 200));
         JTable liabilitiesTable = new JTable(liabilitiesTableModel);
         JScrollPane liabilitiesTableScrollPane = new JScrollPane(liabilitiesTable);
         incomeTableScrollPane.setPreferredSize(new Dimension(265, 195));
 
-        add(liabilitiesTableScrollPane, new GridBagConstraints(0, 0, 1, 1, 1, 1,
+        add(liabilitiesTableScrollPane, new GridBagConstraints(200, 400, 1, 1, 1, 1,
                 GridBagConstraints.SOUTHWEST, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
 
         ExpensesTableModel expensesTableModel = new ExpensesTableModel();
-        incomeTableModel.addIncomeDate(new Expenses("Расходы", 200));
+        expensesTableModel.addExpensesDate(new Expenses("Расходы", 200));
         JTable expensesTable = new JTable(expensesTableModel);
         JScrollPane expensesTableScrollPane = new JScrollPane(expensesTable);
         incomeTableScrollPane.setPreferredSize(new Dimension(265, 195));
 
-        add(expensesTableScrollPane, new GridBagConstraints(0, 0, 1, 1, 1, 1,
+        add(expensesTableScrollPane, new GridBagConstraints(200, 0, 1, 1, 1, 1,
                 GridBagConstraints.SOUTH, GridBagConstraints.HORIZONTAL,
                 new Insets(2, 2, 2, 2), 0, 0));
 
